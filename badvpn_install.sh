389b48a5d592801f9909a158ea7904447eeb3548
Lahiru Sandaruwan Liyanage <72745298+lahirubro123@users.noreply.github.com>
2023-04-19 21:01:59 +0530

    Update badvpn_install.sh

--- badvpn_install.sh
+++ badvpn_install.sh
@@ -8,7 +8,10 @@ fi
 
 # Install dependencies
 apt-get update && apt-get upgrade
+sudo apt-get install pkg-config
 apt-get install -y g++ cmake build-essential python-setuptools git
+sudo apt-get install libnss3-dev
+sudo apt-get install libnspr4-dev
 
 # Clone BadVPN repository
 git clone https://github.com/ambrop72/badvpn.git
